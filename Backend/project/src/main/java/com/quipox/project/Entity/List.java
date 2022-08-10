4881b055ef8868fc06fbffa6f292a8157e4c16c2
felipeg <germanfelipe22@gmail.com>
2022-08-10 07:01:34 -0500

    1. Backend -> H2 BACKEND CONFIGURATION

---
+++ Backend/project/src/main/java/com/quipox/project/Entity/List.java
@@ -0,0 +1,99 @@
+package com.quipox.project.Entity;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+public class List {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long list_id;
+    
+    private String list_name;
+
+   
+
+    @ManyToMany
+    @JoinTable(name="list_songs", joinColumns = @JoinColumn(name="list_id"), inverseJoinColumns = @JoinColumn(name="song_id"))
+    private Set<Song> songs;
+
+   
+
+ 
+
+    //Constructors
+
+    public List() {
+    }
+
+    public List(Long list_id, String list_name, Set<Song> songs) {
+        this.list_id = list_id;
+        this.list_name = list_name;
+        this.songs = songs;
+    }
+
+    
+
+    //Getters and Setters
+
+    public Long getList_id() {
+        return list_id;
+    }
+
+    public void setList_id(Long list_id) {
+        this.list_id = list_id;
+    }
+
+    public String getList_name() {
+        return list_name;
+    }
+
+    public void setList_name(String list_name) {
+        this.list_name = list_name;
+    }
+
+    public Set<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(Set<Song> songs) {
+        this.songs = songs;
+    }
+
+    //Hashcode, equals and toString
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((list_id == null) ? 0 : list_id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        List other = (List) obj;
+        if (list_id == null) {
+            if (other.list_id != null)
+                return false;
+        } else if (!list_id.equals(other.list_id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "List [list_id=" + list_id + ", list_name=" + list_name + ", songs=" + songs + "]";
+    }
+    
+    
+
+}
---
+++ Backend/project/src/main/java/com/quipox/project/Entity/Song.java
@@ -0,0 +1,99 @@
+package com.quipox.project.Entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Song {
+    
+    
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long song_id;
+    private String song_name;
+    private String song_artist;
+    private String song_album;
+    private String song_anno;
+
+    //Constructors
+
+    public Song() {
+    }
+
+    public Song(Long song_id, String song_name, String song_artist, String song_album, String song_anno) {
+        this.song_id = song_id;
+        this.song_name = song_name;
+        this.song_artist = song_artist;
+        this.song_album = song_album;
+        this.song_anno = song_anno;
+    }
+    
+    // Getters and Setters
+
+    public Long getSong_id() {
+        return song_id;
+    }
+    public void setSong_id(Long song_id) {
+        this.song_id = song_id;
+    }
+    public String getSong_name() {
+        return song_name;
+    }
+    public void setSong_name(String song_name) {
+        this.song_name = song_name;
+    }
+    public String getSong_artist() {
+        return song_artist;
+    }
+    public void setSong_artist(String song_artist) {
+        this.song_artist = song_artist;
+    }
+    public String getSong_album() {
+        return song_album;
+    }
+    public void setSong_album(String song_album) {
+        this.song_album = song_album;
+    }
+    public String getSong_anno() {
+        return song_anno;
+    }
+    public void setSong_anno(String song_anno) {
+        this.song_anno = song_anno;
+    }
+
+    //Hashcode, equals and toString
+
+    @Override
+    public String toString() {
+        return "Song [song_album=" + song_album + ", song_anno=" + song_anno + ", song_artist=" + song_artist
+                + ", song_id=" + song_id + ", song_name=" + song_name + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((song_id == null) ? 0 : song_id.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Song other = (Song) obj;
+        if (song_id == null) {
+            if (other.song_id != null)
+                return false;
+        } else if (!song_id.equals(other.song_id))
+            return false;
+        return true;
+    }
+
+
+}
